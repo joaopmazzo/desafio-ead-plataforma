a255b0d64a765cf25175c3c0a43bd5a85e299d0e
João Paulo Mazzo <mazzojp@gmail.com>
2022-01-30 01:46:07 -0300

    Adiciona modal e usa JQuery para fazelo aparecer ou não

--- js/main.js
+++ js/main.js
@@ -1,19 +1,12 @@
-function openModal(modalId) {
-  let modal = document.getElementById(modalId);
+modal.addEventListener('click', function (e) {
+  if (e.target == this)
+    closeModal();
+})
 
-  if (typeof modal == 'undefined' || modal === null)
-    return;
-
-  modal.style.display = 'Block';
-  document.body.style.overflow = 'hidden';
+function openModal() {
+  $('.modal-box').css('display', 'flex');
 }
 
-function closeModal(modalId) {
-  let modal = document.getElementById(modalId);
-
-  if (typeof modal == 'undefined' || modal === null)
-    return;
-
-  modal.style.display = 'none';
-  document.body.style.overflow = 'auto';
+function closeModal() {
+  $('.modal-box').css('display', 'none');
 }
